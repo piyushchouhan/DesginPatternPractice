26915720b6b30d695d74099db9787e689081c0fb
Piyush Chauhan <60496644+piyushchouhan@users.noreply.github.com>
2025-01-01 21:18:19 +0530

    Responsibility Design Pattern Added

---
+++ src/ResponsibilityDesignPattern/LogProcessor.java
@@ -0,0 +1,19 @@
+package ResponsibilityDesignPattern;
+
+public abstract class LogProcessor {
+    public static int INFO = 1;
+    public static int DEBUG = 2;
+    public static int ERROR = 3;
+
+    LogProcessor nextLoggerProcessor;
+
+    LogProcessor(LogProcessor loggerProcessor) {
+        this.nextLoggerProcessor = loggerProcessor;
+    }
+
+    public void log(int logLevel, String message) {
+        if (this.nextLoggerProcessor != null) {
+            this.nextLoggerProcessor.log(logLevel, message);
+        }
+    }
+}
